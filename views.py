f0cfedd9177553d556d03e3127c996b0ace03cb9
Filipe Correia <filipe5675046@aluno.ms.senac.br>
2024-08-01 21:07:30 -0400

    adicionando arquivo de visualização

---
+++ views.py
@@ -0,0 +1,23 @@
+def menu_principal():
+    string = f"""
+               
+                    1 - Humano
+                    2 - Cachorro
+                    3 - cavalo
+                    4 - menu listar
+                    0 - sair
+
+              """
+    return string
+
+def menu_listar():
+    string = f"""
+               
+                    1 - listar humano
+                    2 - listar cachorro
+                    3 - listar cavalo
+                    4 - voltar ao menu principal
+
+              """
+    return string
+
